3c53afaf57c3eaf9a6887d3b40fa70b523324471
achraf ait haddouch <achraf.gm12@gmail.com>
2025-12-23 20:33:59 +0100

    send the fild to git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MainGoal from "./components/MainGoal";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+export default function App() {
+ return(
+  <div id="app">
+    <h1>Time to Practice!</h1>
+      <p>
+        Build a  <span id="mg">&lt;maingoal&gt;</span>  component and insert it below this text.
+      </p>
+      <p>Your <span id="mg">&lt;maingoal&gt;</span> component should simply output some text that describes your main goal <span id="mg">(e.g, 'My main goal: Learn React in great detail").</span>
+      </p>
+      <p >Important : you custom component must contain the next 'my main goal'.</p>
+      <p>Also important : forthe automatic checks to succeed , you must export your custom comonent function ! Not as ad default but simply by adding the export keyword in front of your function (e.g, export function YOUR_COMPONENT_NAME).</p>
+     <MainGoal />
     </div>
-  );
+ )
 }
-
-export default App;
---
+++ src/components/MainGoal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainGoal() {
+  return (
+    <p>Mon objectif principal : Apprendre React depuis la base" en bas</p>
+  )
+}
+
+export default MainGoal
